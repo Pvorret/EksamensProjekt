f719dac36fd1f368a7703faf0a2fb06324179034
Pvorret <phvorret@gmail.com>
2015-04-28 14:22:14 +0200

    Fixede merge

--- EksamensProjekt/Controller/DBFacades/SensorDBFacade.cs
+++ EksamensProjekt/Controller/DBFacades/SensorDBFacade.cs
@@ -12,19 +12,35 @@ namespace EksamensProjekt.Controller.DBFacades
 {
     static class SensorDBFacade
     {
-        SqlConnection SensorSQL = new SqlConnection(DBHelper.DB_conn);
+        static SqlConnection SensorSQL = new SqlConnection(DBHelper.DB_conn);
+        static SqlCommand cmd;
+        static public void CreateSensor(Sensor sensor)
+        {
+            cmd = new SqlCommand("SP_CreateSensor", SensorSQL);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            int activatedToBit;
+
+            if (sensor.Activated == false)
+            {
+                activatedToBit = 0;
+            }
+            else
+            {
+                activatedToBit = 1;
+            }
 
             try
-        {
+            {
                 SensorSQL.Open();
-            
+
                 cmd.Parameters.Add("@S_SerialNumber", sensor.SerialNumber);
                 cmd.Parameters.Add("@S_Type");
-                cmd.Parameters.Add("@S_activated", activatedToBit);        
+                cmd.Parameters.Add("@S_activated", activatedToBit);
                 cmd.Parameters.Add("@S_C_Citizen", "");
                 cmd.Parameters.Add("@S_Location", "");
 
-                cmd.ExecuteNonQuery();                
+                cmd.ExecuteNonQuery();
             }
             catch
             {
