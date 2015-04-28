b6e0b51db0304b69c37745c27a6a96f98c3f9fd7
thom2026 <thomas.b.n@hotmail.com>
2015-04-28 11:55:27 +0200

    CreateSensor view

---
+++ EksamensProjekt/View/CreateSensor.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EksamensProjekt.Controller;
+
+namespace EksamensProjekt.View
+{
+    /// <summary>
+    /// Interaction logic for CreateSensor.xaml
+    /// </summary>
+    public partial class CreateSensor : Window
+    {
+        public CreateSensor()
+        {
+            InitializeComponent();
+            Type_ComboBox.Items.Add("Dør");
+            Type_ComboBox.Items.Add("Fald");
+            Type_ComboBox.Items.Add("Epilepsi");
+            Type_ComboBox.Items.Add("Røg");
+            Type_ComboBox.Items.Add("GPS");
+            Type_ComboBox.Items.Add("Seng");
+        }
+
+
+        private void AddSensor_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Controller.CreateSensor(SerialNumber_TextBox, Type_ComboBox);
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MW = new MainWindow();
+            MW.Show();
+            Close();
+        }
+
+        private void Type_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
