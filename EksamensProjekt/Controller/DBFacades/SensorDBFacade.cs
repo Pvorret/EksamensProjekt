e83378b3d43936d39f5e18d45cb5ea76bff1d9a1
stefangp88 <stefangp88@gmail.com>
2015-04-28 13:24:14 +0200

    SensorDBFAcade.CreateSensor() lavet

--- EksamensProjekt/Controller/DBFacades/SensorDBFacade.cs
+++ EksamensProjekt/Controller/DBFacades/SensorDBFacade.cs
@@ -5,16 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 using EksamensProjekt.Model;
 using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
 
 namespace EksamensProjekt.Controller.DBFacades
 {
     static class SensorDBFacade
     {
-        SqlConnection SensorSQL = new SqlConnection(DBHelper.DB_conn);
-
+        static SqlConnection SensorSQL = new SqlConnection(DBHelper.DB_conn);
+        static SqlCommand cmd;
         static public void CreateSensor(Sensor sensor)
         {
+            cmd = new SqlCommand("SP_CreateSensor", SensorSQL);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            int activatedToBit;
+           
+            if (sensor.Activated == false)
+            {
+                activatedToBit = 0;
+            }
+            else
+            {
+                activatedToBit = 1;
+            }
             
+            try
+            {
+                SensorSQL.Open();
+
+                cmd.Parameters.Add("@S_SerialNumber", sensor.SerialNumber);
+                cmd.Parameters.Add("@S_Type");
+                cmd.Parameters.Add("@S_activated", activatedToBit);        
+                cmd.Parameters.Add("@S_C_Citizen", "");
+                cmd.Parameters.Add("@S_Location", "");
+
+                cmd.ExecuteNonQuery();                
+            }
+            catch
+            {
+                MessageBox.Show("Error! Sensor not added to database");
+            }
+            finally
+            {
+                SensorSQL.Close();
+            }
         }
     }
 }
