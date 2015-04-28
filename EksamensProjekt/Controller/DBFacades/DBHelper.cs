392053a6943014855e5d7a30e86f2545a49a81e8
stefangp88 <stefangp88@gmail.com>
2015-04-28 12:12:23 +0200

    DBHelper

---
+++ EksamensProjekt/Controller/DBFacades/DBHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.Controller.DBFacades
+{
+    public static class DBHelper
+    {
+        public static string DB_conn = "Server=ealbd1.eal.local;Database=EJL22_DB;User Id=ejl22_usr;Password=Baz1nga22";
+    }
+}
