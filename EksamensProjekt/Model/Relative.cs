1f68593c6e3cad9ad9e8d9ec899fd2d320174c5c
Pvorret <phvorret@gmail.com>
2015-04-28 12:54:58 +0200

    Tilføjet klasser

---
+++ EksamensProjekt/Model/Relative.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.Model
+{
+    class Relative : Person
+    {
+        public string Email { get; set; }
+        public TimeSpan NotAvailable { get; set; }
+
+        public Relative(string name, string cprNr, string gender, string age, string address, string city, string phoneNumber, string email, List<string> NotAvailable)
+        {
+            this.Name = name;
+            this.CprNr = cprNr;
+
+        }
+    }
+}
