da6eece860d3d1af1d24c9c887b96b86af78dad8
Pvorret <phvorret@gmail.com>
2015-04-29 10:36:40 +0200

    Lidt i Controller

--- EksamensProjekt/Controller/CitizenController.cs
+++ EksamensProjekt/Controller/CitizenController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EksamensProjekt.Model;
 
 namespace EksamensProjekt.Controller {
     class CitizenController {
+        private List<Relative> Relatives = new List<Relative>(); 
+
+
     }
 }
