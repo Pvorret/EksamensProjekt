b4c6d2fa575010652cc4ccae6b1e1230bb6df809
Pvorret <phvorret@gmail.com>
2015-05-06 14:13:56 +0200

    Så

---
+++ EksamensProjekt/Controller/DBFacades/RuleSetDBFacade.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using EksamensProjekt.Model;
+
+namespace EksamensProjekt.Controller.DBFacades
+{
+    class RuleSetDBFacade
+    {
+        static SqlConnection dbconn;
+        private static void ConnectDB()
+        {
+            dbconn = new SqlConnection(DBHelper._connectionString);
+            dbconn.Open();
+        }
+        private static void CloseDB()
+        {
+            dbconn.Close();
+            dbconn.Dispose();
+        }
+
+        public static SensorLog CreateSensorLog(SensorLog sl)
+        {
+            SensorLog sensorLog = new SensorLog(sl.SensorSerialNumber, sl.ActivationTime);
+            try
+            {
+                ConnectDB();
+                SqlCommand cmd = new SqlCommand("SP_CreateSensorLog", dbconn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@SerialNumber", sl.SensorSerialNumber));
+                cmd.Parameters.Add(new SqlParameter("@ActivationTime", sl.ActivationTime));
+                SqlDataReader rdr = cmd.ExecuteReader();
+                
+                while(rdr.HasRows && rdr.Read())
+                {
+                    sensorLog.ID = Convert.ToInt32(rdr["SL_ID"]);
+                }
+            }
+            catch (SqlException e)
+            {
+                throw new Exception("Error! Sensor not added to database" + e.Message);
+            }
+            finally
+            {
+                CloseDB();
+            }
+            return sensorLog;
+        }
+        public static void UpdateSensorLog(SensorLog sl)
+        {//This shit
+            try
+            {
+                ConnectDB();
+                SqlCommand cmd = new SqlCommand("SP_UpdateSensorLog", dbconn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@ID", sl.ID));
+                cmd.Parameters.Add(new SqlParameter("@ContactPerson", sl.ContactPerson));
+                cmd.Parameters.Add(new SqlParameter("@ContactTime", sl.ContactTime));
+                cmd.Parameters.Add(new SqlParameter("@ContactMessage", sl.ContactMessage));
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception("Error! Sensor not added to database" + e.Message);
+            }
+            finally
+            {
+                CloseDB();
+            }
+        }
+
+    }
+}
