7b80b8efff369cc19f3d5caa2739b062482e17a7
Pvorret <phvorret@gmail.com>
2015-05-06 13:38:10 +0200

    sensorlog klasse

---
+++ EksamensProjekt/Model/SensorLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.Model
+{
+    class SensorLog
+    {
+        public int ID { get; set; }
+        public int SensorSerialNumber { get; set; }
+        public DateTime ActivationTime { get; set; }
+        public DateTime ContactTime { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactMessage { get; set; }
+
+        public SensorLog(int id, int sensorSerialNumber, DateTime activationTime, DateTime contactTime, string contactPerson, string contactMessage)
+        {
+            this.ID = id;
+            this.SensorSerialNumber = sensorSerialNumber;
+            this.ActivationTime = activationTime;
+            this.ContactTime = contactTime;
+            this.ContactPerson = contactPerson;
+            this.ContactMessage = contactMessage;
+        }
+        public SensorLog(int id, int sensorSerialNumber, DateTime activationTime)
+        {
+            this.ID = id;
+            this.SensorSerialNumber = sensorSerialNumber;
+            this.ActivationTime = activationTime;
+        }
+    }
+}
