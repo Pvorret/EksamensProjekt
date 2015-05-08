28b0dad095d398a38b314f274d861e90656b47f6
NicolajPedersen <nicolajgpedersen@gmail.com>
2015-05-08 11:14:57 +0200

    Hej hej hej hej hej hej hej hej

---
+++ UnitTestBehandleSensorInput/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EksamensProjekt;
+using EksamensProjekt.Controller.DBFacades;
+
+namespace UnitTestBehandleSensorInput {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
