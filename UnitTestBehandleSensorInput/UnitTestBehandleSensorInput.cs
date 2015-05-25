95feccff4e764e49262c1fd951c7b07d827ebd54
stefangp88 <stefangp88@gmail.com>
2015-05-25 10:03:45 +0200

    unittest behandlesensorinput

--- UnitTestBehandleSensorInput/UnitTestBehandleSensorInput.cs
+++ UnitTestBehandleSensorInput/UnitTestBehandleSensorInput.cs
@@ -9,9 +9,12 @@ namespace UnitTest {
         public void TestMethod1() 
         {
             RuleSetController RSC = new RuleSetController();
-            DateTime DT = new DateTime(2015,12,14,13,24,00,00);
-            RSC.HandelSensorInput(23323, DT);//dfsdf
-            
+            DateTime ActivationTime = new DateTime(2015, 05, 15, 10, 12, 00, 00);
+            RSC.HandelSensorInput(1, ActivationTime);
+            Assert.AreEqual(RSC.ContactList[0], RSC.TimeRangeRules[0].RelativeCPRNR);
+            Assert.AreEqual(true, RSC.Contact);
+            Assert.AreEqual("Helper", RSC.ContactList[0]);
+            Assert.AreEqual("SR 69", RSC.TimeRangeRules[0].ActingRule);// skal lige ændres til et rigtig værdi
         }
     }
 }
