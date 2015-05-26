d6347e4abb9f7820287b67af85bdc995a84e70c9
NicolajPedersen <nicolajgpedersen@gmail.com>
2015-05-26 23:00:03 +0200

    Har lavet lidt til UnitTestFortoførsteUseCases

--- UnitTestBehandleSensorInput/UnitTestFortoførsteUseCases.cs
+++ UnitTestBehandleSensorInput/UnitTestFortoførsteUseCases.cs
@@ -9,33 +9,97 @@ using System.Collections.Generic;
 namespace UnitTest {
     [TestClass]
     public class UnitTestFortoførsteUseCases {
-        [TestMethod]
-        public void TestAddandGetSensorRuleFromSerialNumber() {
-            RuleSetController rulesetcontroller = new RuleSetController();
-            SensorController sensorcontroller = new SensorController();
-            CitizenController citizencontroller = new CitizenController();
-            //Random random = new Random();
+        RuleSetController rulesetcontroller = new RuleSetController();
+        SensorController sensorcontroller = new SensorController();
+        CitizenController citizencontroller = new CitizenController();
 
-            //Test data i databasen.
+        [TestMethod]
+        public void TestGetAllCitizen() {
+            string Name = "Test Testensen";
+            string CPRNR = "111111-2222";
+
+            citizencontroller.GetAllCitizen();
+            foreach (Citizen c in citizencontroller.Citizens) {
+                Assert.AreEqual(c.Name, Name);
+                Assert.AreEqual(c.CprNr, CPRNR);
+            }
+        }
+
+        [TestMethod]
+        public void TestCreateCitizen() {
+
+        }
+
+        [TestMethod]
+        public void TestGetRelativeFromCitizen() {
+            string CitizenCPRNR = "111111-2222";
+            string RelativeName = "Testensen";
+            string RelativeCPRNR = "222222-1111";
+            //Inde i CitizenDatabseFacaden i GetRelativeFromCitizen, hvorfor er der et forloop som løber en liste igennem og så add til listen hvis der er noget i, og bagefter hvis der så ikke er noget i så lægger den også i.
+
+            citizencontroller.GetAllCitizen();
+            citizencontroller.GetRelativeFromCitizen(CitizenCPRNR);
+            foreach (Citizen c in citizencontroller.Citizens) {
+                foreach (Relative r in c.Relatives) {
+                    Assert.AreEqual(r.Name, RelativeName);
+                    Assert.AreEqual(r.CprNr, RelativeCPRNR);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void AddRelative() {
+
+        }
+
+        [TestMethod]
+        public void TestGetSensor() {
+            int getSerialNumber = 888888;
+            string sensorType = "Test";
+
+            Assert.AreEqual(getSerialNumber, SensorDBFacade.GetSensor(getSerialNumber)[0].SerialNumber);
+            Assert.AreEqual(sensorType, SensorDBFacade.GetSensor(getSerialNumber)[0].Type);
+        }
+
+        [TestMethod]
+        public void TestCreateSensor() {
+            int serialNumber = 111111;
+            string sensorType2 = "Dør";
+
+            sensorcontroller.CreateSensor(serialNumber, sensorType2);
+
+            foreach (Sensor s in SensorDBFacade.GetSensor(serialNumber)) {
+                Assert.AreEqual(s.SerialNumber, serialNumber);
+                Assert.AreEqual(s.Type, sensorType2);
+            }   
+        }
+
+        [TestMethod]
+        public void TestConnectSensorToCitizen() {
+            int serialNumber = 111111;
+            string CitizenCPRNR = "111111-2222";
+            string SensorLocation = "Hoveddør";
+            sensorcontroller.ConnectSensorToCitizen(serialNumber, CitizenCPRNR, SensorLocation);
+            citizencontroller.GetSensorsFromCitizen(CitizenCPRNR);
+            foreach (Citizen c in citizencontroller.Citizens) { //Ved ikke om man kan gøre sådan her?
+                if (c.CprNr == CitizenCPRNR) {
+                    foreach (Sensor s in c.Sensors) {
+                        Assert.AreEqual(s.SerialNumber, serialNumber);
+                        Assert.AreEqual(s.Location, SensorLocation);
+                    }
+                }
+                
+            }
+        }
+
+        [TestMethod]
+        public void TestGetSensorRuleFromSerialNumber() {
             int getSerialNumber = 888888;
             int sensorDependency1 = 999999;
             bool waitOrLook = true;
             int timeToWait = 2;
             int timeToLook = 0;
             bool whenToSend = true;
-            string sensorType = "Test";
-
-            //Test data til at ligge i databasen.
-            int serialNumber = 111111;
-            int sensorDependency2 = 111111;
-            bool waitOrLook2 = true;
-            int timeToWait2 = 2;
-            int timeToLook2 = 1;
-            bool whenToSend2 = false;
-            string sensorType2 = "Dør";
-            int sensorrulemanagementid2 = 0;
-
-            //Når man adder sensorrulemanagement til databasen, retunere selve add metoden sensorrulemanagementid, som så skal bruges i AddSensorRuleFromSerialNumber.
 
             foreach (SensorRule sr in rulesetcontroller.GetSensorRuleFromSerialNumber(getSerialNumber)) {
                 Assert.AreEqual(sr.SensorDependency, sensorDependency1);
@@ -44,26 +108,27 @@ namespace UnitTest {
                 Assert.AreEqual(sr.TimeToLook, timeToLook);
                 Assert.AreEqual(sr.WhenToSend, whenToSend);
             }
+        }
 
-            Assert.AreEqual(getSerialNumber, SensorDBFacade.GetSensor(getSerialNumber)[0].SerialNumber);
-            Assert.AreEqual(sensorType, SensorDBFacade.GetSensor(getSerialNumber)[0].Type);
+        [TestMethod]
+        public void TestAddSensorRuleFromSerialNumber() {
+            int serialNumber = 111111;
+            int sensorDependency2 = 888888;
+            bool waitOrLook2 = true;
+            int timeToWait2 = 1;
+            int timeToLook2 = 0;
+            bool whenToSend2 = false;
+            string RuleSet = "SR";
+            int sensorrulemanagementid2 = 0;
+            
+            rulesetcontroller.AddSensorRuleManagement(RuleSet, serialNumber);
 
-            sensorcontroller.CreateSensor(serialNumber, sensorType2);
+            sensorrulemanagementid2 = rulesetcontroller.SensorRuleManagementId;
 
-            foreach (Sensor s in SensorDBFacade.GetSensor(0)) {
-                if (s.SerialNumber == serialNumber && s.Type == sensorType2) {
-                    Assert.AreEqual(s.SerialNumber, serialNumber);
-                    Assert.AreEqual(s.Type, sensorType);
-                }
-            }
-
-            //sensorcontroller.ConnectSensorToCitizen();
-            //citizencontroller.GetSensorsFromCitizen();
-
+            //Når man adder sensorrulemanagement til databasen, retunere selve add metoden sensorrulemanagementid, som så skal bruges i AddSensorRuleFromSerialNumber.
             //sensorrulemanagementid2 = sensrulesetcontroller.AddSensorRuleManagement("SR", serialNumber);
             //Skal også lige gøres for en timerangerule. Men det hele skal deles op i forhold til SR og TRR.
-
-
+            
             rulesetcontroller.AddSensorRuleFromSerialNumber(serialNumber, sensorDependency2, waitOrLook2, timeToWait2, timeToLook2, whenToSend2, sensorrulemanagementid2);
 
             foreach (SensorRule sr in rulesetcontroller.GetSensorRuleFromSerialNumber(serialNumber)) {
@@ -73,9 +138,43 @@ namespace UnitTest {
                 Assert.AreEqual(sr.TimeToLook, timeToLook2);
                 Assert.AreEqual(sr.WhenToSend, whenToSend2);
             }
+        }
 
+        [TestMethod]
+        public void TestAddTimeRangeRuleFormSerialNumber() {
+            int serialNumber = 111111;
+            string Day = "Monday";
+            DateTime StartTime = Convert.ToDateTime("08:00");
+            DateTime EndTime = Convert.ToDateTime("10:00");
+            string RelativeCPRNR = "222222-1111"; //Ved heller ikke rigtig hvad jeg skal gøre med Relative, når jeg ikke rigtig kan teste på Get og Add - Relative.
+            int SensorRuleId = 0;
+            string ActingRule = "SR" + SensorRuleId;
+            bool ContactHelper = true;
+            int SensorSerialNumber = 0; //Skal lige have fundet ud af hvad jeg gør her, om det bare skal være en som også hele tiden ligger ude i Databasen?
+            int sensorDependency2 = 888888;
+            bool waitOrLook2 = false;
+            int timeToWait2 = 0;
+            int timeToLook2 = 1;
+            bool whenToSend2 = true;
+            int sensorrulemanagementid2 = 0;
+            string RuleSet = "TRR";
+
+            //Jeg for står ikke inde i vinduet AddSensorRuleOrTimeRangeRule, at hvis man har valgt sensor regel = TRR og man så her efter skal koble en sensor regle til.
+            //at man så i koden opretter en SensorRule? Linje 182 og så derefter opretter en TimeRangeRule, for så det jo kun den regel man har at vælge i mellem.
+
+            rulesetcontroller.AddSensorRuleManagement(RuleSet, serialNumber);
+            sensorrulemanagementid2 = rulesetcontroller.SensorRuleManagementId;
+            rulesetcontroller.AddSensorRuleFromSerialNumber(serialNumber, sensorDependency2, waitOrLook2, timeToWait2, timeToLook2, whenToSend2, sensorrulemanagementid2);
+            SensorRuleId = rulesetcontroller.SensorRuleId;
+            rulesetcontroller.AddTimeRangeRuleFromSerialNumber(serialNumber, Day, StartTime, EndTime, RelativeCPRNR, ActingRule, ContactHelper);
+        }
+
+        [TestMethod]
+        public void DeleteSensor() {
+            int serialNumber = 111111;
             sensorcontroller.DeleteSensor(serialNumber);
 
+            Assert.AreEqual(serialNumber, SensorDBFacade.GetSensor(serialNumber)[0].SerialNumber);
         }
 
         [TestMethod]
